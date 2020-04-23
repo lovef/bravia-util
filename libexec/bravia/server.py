42324788bf1a117ed0c94aae1028b4ed06dc5d8f
Love F <lovef.code@gmail.com>
2020-04-23 21:51:03 +0200

    python command

--- libexec/bravia/server.py
+++ libexec/bravia/server.py
@@ -3,20 +3,42 @@
 import urllib.request
 import json
 import setup as setup_script
+import access
 import time
 from common import printerr
 
 ip = None
+cookie = None
 
 def setup(script):
     global ip
     ip = setup_script.main(script)
+
+def setupAccess(script):
+    setup(script)
+    global cookie
+    cookie = access.main(script)
 
 def get(path, jsonData):
     data = json.dumps(jsonData).encode("utf-8")
     req = urllib.request.Request(f"http://{ip}/{path}", data)
     response = sendRequest(req, "GET " + path)
     return json.loads(response.read())
+
+def command(command, code):
+    data = f"""<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+        <s:Body>
+            <u:X_SendIRCC xmlns:u="urn:schemas-sony-com:service:IRCC:1">
+                <IRCCCode>{code}</IRCCCode>
+            </u:X_SendIRCC>
+        </s:Body>
+    </s:Envelope>""".encode("ascii")
+    headers = {
+        "Cookie": cookie,
+        "SOAPACTION": '"urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"'
+    }
+    req = urllib.request.Request(f"http://{ip}/sony/ircc", data, headers, method="POST")
+    response = sendRequest(req, f"IRCC {command} {code}")
 
 def requestCookie(url, data, auth = None):
     headers = { "Authorization": auth } if auth is not None else {}
