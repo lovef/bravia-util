fc08733c831d41aa5ef8bc052643c8ad6a9ba5d0
Love F <lovef.code@gmail.com>
2020-05-10 18:43:53 +0200

    Color log

--- bin/lib/bravia/common.py
+++ bin/lib/bravia/common.py
@@ -1,8 +1,54 @@
 import sys
 import datetime
 
+class color:
+    END       = '\33[0m'
+    BOLD      = '\33[1m'
+    ITALIC    = '\33[3m'
+    UNDERLINE = '\33[4m'
+    BLINK     = '\33[5m'
+    BLINK2    = '\33[6m'
+    SELECTED  = '\33[7m'
+
+    BLACK  = '\33[30m'
+    RED    = '\33[31m'
+    GREEN  = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE   = '\33[34m'
+    VIOLET = '\33[35m'
+    CYAN   = '\33[36m'
+    WHITE  = '\33[37m'
+
+    GREY          = '\33[90m'
+    BRIGHT_RED    = '\33[91m'
+    BRIGHT_GREEN  = '\33[92m'
+    BRIGHT_YELLOW = '\33[93m'
+    BRIGHT_BLUE   = '\33[94m'
+    BRIGHT_VIOLET = '\33[95m'
+    BRIGHT_CYAN   = '\33[96m'
+    BRIGHT_WHITE  = '\33[97m'
+
+    class bg:
+        BLACK  = '\33[40m'
+        RED    = '\33[41m'
+        GREEN  = '\33[42m'
+        YELLOW = '\33[43m'
+        BLUE   = '\33[44m'
+        VIOLET = '\33[45m'
+        CYAN   = '\33[46m'
+        WHITE  = '\33[47m'
+
+        GREY          = '\33[100m'
+        BRIGHT_RED    = '\33[101m'
+        BRIGHT_GREEN  = '\33[102m'
+        BRIGHT_YELLOW = '\33[103m'
+        BRIGHT_BLUE   = '\33[104m'
+        BRIGHT_VIOLET = '\33[105m'
+        BRIGHT_CYAN   = '\33[106m'
+        BRIGHT_WHITE  = '\33[107m'
+
 def timestamp():
-    return datetime.datetime.now().isoformat(sep=' ', timespec='milliseconds')
+    return color.GREY + datetime.datetime.now().isoformat(sep=' ', timespec='milliseconds') + color.END
 
 def printerr(*args, prefix = timestamp(), **kwargs):
     if prefix is None:
--- bin/lib/bravia/input.py
+++ bin/lib/bravia/input.py
@@ -3,52 +3,7 @@ import json
 from . import server
 from . import config
 from .common import printerr
-
-class color:
-    END       = '\33[0m'
-    BOLD      = '\33[1m'
-    ITALIC    = '\33[3m'
-    UNDERLINE = '\33[4m'
-    BLINK     = '\33[5m'
-    BLINK2    = '\33[6m'
-    SELECTED  = '\33[7m'
-
-    BLACK  = '\33[30m'
-    RED    = '\33[31m'
-    GREEN  = '\33[32m'
-    YELLOW = '\33[33m'
-    BLUE   = '\33[34m'
-    VIOLET = '\33[35m'
-    CYAN   = '\33[36m'
-    WHITE  = '\33[37m'
-
-    GREY          = '\33[90m'
-    BRIGHT_RED    = '\33[91m'
-    BRIGHT_GREEN  = '\33[92m'
-    BRIGHT_YELLOW = '\33[93m'
-    BRIGHT_BLUE   = '\33[94m'
-    BRIGHT_VIOLET = '\33[95m'
-    BRIGHT_CYAN   = '\33[96m'
-    BRIGHT_WHITE  = '\33[97m'
-
-    class bg:
-        BLACK  = '\33[40m'
-        RED    = '\33[41m'
-        GREEN  = '\33[42m'
-        YELLOW = '\33[43m'
-        BLUE   = '\33[44m'
-        VIOLET = '\33[45m'
-        CYAN   = '\33[46m'
-        WHITE  = '\33[47m'
-
-        GREY          = '\33[100m'
-        BRIGHT_RED    = '\33[101m'
-        BRIGHT_GREEN  = '\33[102m'
-        BRIGHT_YELLOW = '\33[103m'
-        BRIGHT_BLUE   = '\33[104m'
-        BRIGHT_VIOLET = '\33[105m'
-        BRIGHT_CYAN   = '\33[106m'
-        BRIGHT_WHITE  = '\33[107m'
+from .common import color
 
 def setPreset(preset):
     print("set preset", preset)
@@ -65,6 +20,7 @@ def listInputs():
 
     inputs = response['result'][0]
     inputs = sorted(inputs, key=lambda i: i['connection'], reverse=True)
+    preset = config.readPreset()
     print("Commands to change preset:")
     for inputDef in inputs:
         label = inputDef['label']
@@ -75,7 +31,11 @@ def listInputs():
         uri = inputDef['uri']
         quotedUri = f"'{uri}'"
 
-        print(f"  {activeStr}bravia --input {quotedUri:50} # {label:2} {title} {color.END}")
+        inputString = f"  {activeStr}bravia --input {quotedUri:50} # {label:2} {title} {color.END}"
+        if preset == uri:
+            preset = inputString
+        else:
+            print(inputString)
 
     print("Current preset")
-    print(config.readPreset())
+    print(preset)
