6c906a310e557abf1401499f2ffb5a62912e372f
Love F <lovef.code@gmail.com>
2020-11-07 18:46:55 +0100

    Extended interactive options

--- bin/lib/bravia/interactive.py
+++ bin/lib/bravia/interactive.py
@@ -4,19 +4,25 @@ from . import keyboard
 from . import server
 from . import command
 from .common import printerr
+from .input import listInputs
 
 def main():
     server.setupAccess()
     print("Interactive mode")
-    print("Press Escape or Q to exit")
+    print("Press Escape or Q to exit, type ? to list commands")
     keyBoard = keyboard.KeyBoard()
     commandTable = {
+        '?': 'list commands',
+        'i': 'list inputs',
+        's': 'status',
         'up': 'up',
         'down': 'down',
         'left': 'left',
         'right': 'right',
         'home': 'home',
         'delete': 'tvPower',
+        'p': 'picOff',
+        'end': 'turn off screen',
         'enter': 'dpadCenter',
         'backspace': 'exit',
         '-': 'volumeDown',
@@ -33,5 +39,16 @@ def main():
         if c == 'escape' or c == 'q':
             print()
             return
-        if c in commandTable:
+        elif c == '?':
+            print("Command table:")
+            for e in commandTable:
+                print(f" {e:10} {commandTable[e]}")
+        elif c == 's':
+            print(server.powerStatus())
+            print(server.powerSavingMode())
+        elif c == 'end':
+            server.turnOffScreen()
+        elif c == 'i':
+            listInputs()
+        elif c in commandTable:
             command.command(commandTable[c])
